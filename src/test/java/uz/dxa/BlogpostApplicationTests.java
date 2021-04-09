138ea14b0fa52121849295630f3955133039dfea
Mirfayz Barotov <mirik220798@gmail.com>
2021-04-09 15:21:25 +0500

    Blogpost created

---
+++ src/test/java/uz/dxa/BlogpostApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.dxa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
